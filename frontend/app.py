81060a807656c07d7e04eb7b5da5ab9af28efb07
trivedi25 <85929415+trivedi25@users.noreply.github.com>
2024-08-28 13:38:04 +0530

    Create app.py

---
+++ frontend/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    data = request.json
+    return jsonify(data)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
